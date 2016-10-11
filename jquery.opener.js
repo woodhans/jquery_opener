f76366d2ec6d2dd6aaab58e155b75cc1316dbf67
hans.wu <woodhans1@hotmail.com>
2016-10-11 17:14:21 +0800

    v1.0.1

--- jquery.opener.js
+++ jquery.opener.js
@@ -8,21 +8,44 @@
 				var base=this;
 				base.$opt=options;
 				base.$elem=el;
-				base.setup()
+				base._setup()
 			},
-			setup:function(){
+			_setup:function(){
 				var base=this;
 				var _width=typeof(base.$opt.width)=="string"?base.$opt.width:base.$opt.width+"px",
 					_height=typeof(base.$opt.height)=="string"?base.$opt.height:base.$opt.height+"px",
 					_radius=base.$opt.radius>0?"radius:"+base.$opt.radius+"px; -webkit-radius:"+base.$opt.radius+"px;-moz-radius:"+base.$opt.radius+"px;":"",
-					_overfl=typeof(base.$opt.overflow)=="string"?"overflow:"+base.$opt.overflow+";":getOverflow(base.$opt.overflow);
-					_bgcolor=typeof(base.$opt.bgcolor)=="string"?base.$opt.bgcolor:Hex2Color(base.$opt.bgcolor);
-				
-				titleHtml=base.getTitle();
-				imgHtml=base.getImg();
-				contentHtml=base.getContent();
-				
+					_overfl=typeof(base.$opt.overflow)=="string"?"overflow:"+base.$opt.overflow+";":_getOverflow(base.$opt.overflow);
+					_bgcolor=typeof(base.$opt.bgcolor)=="string"?base.$opt.bgcolor:_Hex2Color(base.$opt.bgcolor);
+
+				titleHtml=base._getTitle();
+				imgHtml=base._getImg();
+				contentHtml=base._getContent();
 				var html='<div class="hw_opener" style="width:'+_width+"; height:"+_height+"; background-color:"+_bgcolor+";"+_radius+_overfl+"z-index:"+base.$opt.zIndex+';" id="'+base.$opt.id+'">'+titleHtml+imgHtml+contentHtml+"</div><div class='hw_mask'></div>";
+				base.$elem.on("click",function(){
+					if(!base.$opt.before && typeof (base.$opt.before)=="function") base.$opt.before();
+					base._generateHtml(html);
+				})
+
+				if(!base.$opt.success && typeof (base.$opt.success)=="function"){
+					setTimeout(base.$opt.success,100);
+				}
+
+			},
+			_getTitle:function(){
+				var base=this;
+				return base.$opt.title!=""?"<div class='hw_title'><h3>"+base.$opt.title+"</h3></div>":"";
+
+			},
+			_getContent:function(){
+				var base=this;
+				return base.$opt.content!=""?"<div class='hw_content'><p>"+base.$opt.content+"</p></div>":""
+			},
+			_getImg:function(){
+				var base=this;return base.$opt.img!=""?"<div class='hw_imgs'>"+_getImgs(base.$opt.img)+"</div>":""
+			},
+			_generateHtml:function(html){
+				var base=this;
 				$("body").append(html);
 				$opener=$('.hw_opener');
 				half_lft=$opener.width()/2*-1;
@@ -70,26 +93,10 @@
 						$opener.css({"left":"50%","margin-left":half_lft,"top":"50%","margin-top":half_top});
 					}
 				}
-				if(!base.$opt.success && typeof (base.$opt.success)=="function"){
-					setTimeout(base.$opt.success,100);
-				}
 				$('.hw_mask').on('click',function(){
 					base.remove();
 				})
 			},
-			getTitle:function(){
-				var base=this;
-				return base.$opt.title!=""?"<div class='hw_title'><h3>"+base.$opt.title+"</h3></div>":"";
-				
-			},
-			getContent:function(){
-				var base=this;
-				return base.$opt.content!=""?"<div class='hw_content'><p>"+base.$opt.content+"</p></div>":""
-			},
-			getImg:function(){
-				var base=this;return base.$opt.img!=""?"<div class='hw_imgs'>"+getImgs(base.$opt.img)+"</div>":""
-			},
-			
 			remove:function(){
 				var base=this;
 				$('.hw_mask').remove();
@@ -99,11 +106,11 @@
 	};
 	$.fn.opener=function(options){
 		options=$.extend({},$.fn.opener.options,this.data(),options);
-		if(options.id==""){options.id=randomString(4,8)}
-		
-		if(typeof (options.img) =="string" && options.img.indexOf("[")>-1&&options.img.indexOf("]")>1){options.img=eval("("+options.img+")");}
-		if(typeof (options.overflow) =="string" && options.overflow.indexOf("{")>-1&&options.overflow.indexOf("}")>1){options.overflow=eval("("+options.overflow+")");}
-		
+		if(options.id==""){options.id=_randomString(4,8)}
+
+		if(typeof (options.img) =="string" && options.img.indexOf(",")>1) options.img=options.img.split(",");
+		if(typeof (options.overflow) =="string" && /^\{+[x|y]:[scroll|hidden|auto|visible|inherit|overlay|initial]+(,[x|y]:[scroll|hidden|auto|visible|inherit|overlay|initial]+)?\}$/.test(options.overflow)) options.overflow=eval("("+options.overflow+")");
+
 		var op=new Opener();
 		op.init(options,this)
 	};
@@ -114,14 +121,20 @@
 		img:"",
 		content:"",
 		title:"",
-		overflow:"scroll",
+		overflow:"auto",
 		radius:0,
 		zIndex:9999,
 		id:"",
+		animation:!!0,
+		async:!!0,
 		bgcolor:0xffffff,
+		before:null,
 		success:null,
+		closed:null,
+		beforeClose:null
 	};
-	function getImgs(imgs){
+
+	function _getImgs(imgs){
 		var res="";
 		if(typeof imgs=="string"){
 			res="<img src='"+imgs+"' />"
@@ -131,7 +144,7 @@
 			}
 		return res
 	}
-	function getOverflow(o){
+	function _getOverflow(o){
 		var res="";
 		if(!o.x){
 			res+="overflow-x:"+o.x+";"
@@ -141,7 +154,7 @@
 		}
 		return res
 	}
-	function randomString(minlen,maxlen){
+	function _randomString(minlen,maxlen){
 		var _rnd,
 		maxlen=maxlen?maxlen:minlen,
 		str=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",0,1,2,3,4,5,6,7,8,9],
@@ -154,13 +167,12 @@
 			if(i>minlen){
 				if(Math.round(Math.random()))
 					return _arr.join("")
-				
 			}
 			_arr.push(str[_rnd])
 		}
 		return _arr.join("")
 	}
-	function Hex2Color(hex){
+	function _Hex2Color(hex){
 		return "#"+hex.toString(16)
 	}
 }(jQuery,window,document));
